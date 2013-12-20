561f9090d239b291601b5ff14997b7c04fc06af6
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2013-12-20 14:40:42 +0100

    Adding more performance tests.

--- src/test/scala/org/reactress/bench/HashTablePerformanceBench.scala
+++ src/test/scala/org/reactress/bench/HashTablePerformanceBench.scala
@@ -13,13 +13,35 @@ class HashTablePerformanceBench extends PerformanceTest.Regression {
   def persistor = Persistor.None
 
   val hashTableSizes = Gen.range("size")(10000, 50000, 10000)
+  val reactTables = for {
+    sz <- hashTableSizes
+  } yield {
+    val m = ReactTable[Int, Int]
+    for (i <- 1 until sz) m(i) = 0
+    m
+  }
+  val reactSets = for {
+    sz <- hashTableSizes
+  } yield {
+    val m = ReactSet[Int]
+    for (i <- 1 until sz) m += i
+    m
+  }
+  val hashSets = for {
+    sz <- hashTableSizes
+  } yield {
+    val m = mutable.HashSet[Int]()
+    for (i <- 1 until sz) m += i
+    m
+  }
 
   performance of "insert" config(
     exec.minWarmupRuns -> 30,
     exec.maxWarmupRuns -> 60,
     exec.benchRuns -> 36,
     exec.independentSamples -> 3,
-    exec.outliers.suspectPercent -> 50
+    exec.outliers.suspectPercent -> 60,
+    exec.reinstantiation.frequency -> 6
   ) in {
 
     measure method "ReactMap" in {
@@ -38,11 +60,25 @@ class HashTablePerformanceBench extends PerformanceTest.Regression {
       }
     }
 
+    measure method "ReactTable-stable" in {
+      using(reactTables) curve("ReactTable") in { m =>
+        val sz = m.size
+        for (i <- 1 until sz) m(i) = i
+      }
+    }
+
     measure method "ReactSet" in {
       using(hashTableSizes) curve("ReactSet") in { sz =>
         val m = ReactSet[Int]
         for (i <- 1 until sz) m += i
         m
+      }
+    }
+
+    measure method "ReactSet-stable" in {
+      using(reactSets) curve("ReactSet") in { m =>
+        val sz = m.size
+        for (i <- 1 until sz) m += i
       }
     }
 
@@ -59,6 +95,13 @@ class HashTablePerformanceBench extends PerformanceTest.Regression {
         val m = mutable.Set[Int]()
         for (i <- 1 until sz) m += i
         m
+      }
+    }
+
+    measure method "mutable.HashSet-stable" in {
+      using(hashSets) curve("mutable.HashSet") in { m =>
+        val sz = m.size
+        for (i <- 1 until sz) m += i
       }
     }
   }
