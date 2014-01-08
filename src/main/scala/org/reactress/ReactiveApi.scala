2807d821240bf4ff92630966275ce6ea32f4f4c5
U-phoenix\axel22 <axel22@gmail.com>
2014-01-08 02:19:40 +0100

    Fix missing event propagations.

--- src/main/scala/org/reactress/ReactiveApi.scala
+++ src/main/scala/org/reactress/ReactiveApi.scala
@@ -9,11 +9,12 @@ class ReactiveApi {
   }
 
   object Tuple2Extensions {
-    class Mutate[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S, M](tuple: (Signal[T], Signal[S]), mutable: M, f: (T, S) => Unit)
+    class Mutate[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S, M <: ReactMutable](tuple: (Signal[T], Signal[S]), mutable: M, f: (T, S) => Unit)
     extends Reactive.ProxySubscription {
       val m1 = new Reactor[T] {
         def react(value: T) {
           f(tuple._1(), tuple._2())
+          mutable.onMutated()
         }
         def unreact() {
         }
@@ -21,6 +22,7 @@ class ReactiveApi {
       val m2 = new Reactor[S] {
         def react(value: S) {
           f(tuple._1(), tuple._2())
+          mutable.onMutated()
         }
         def unreact() {
         }
@@ -37,11 +39,12 @@ class ReactiveApi {
   }
 
   object Tuple3Extensions {
-    class Mutate[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S, @spec(Int, Long, Double) U, M](tuple: (Signal[T], Signal[S], Signal[U]), mutable: M, f: (T, S, U) => Unit)
+    class Mutate[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S, @spec(Int, Long, Double) U, M <: ReactMutable](tuple: (Signal[T], Signal[S], Signal[U]), mutable: M, f: (T, S, U) => Unit)
     extends Reactive.ProxySubscription {
       val m1 = new Reactor[T] {
         def react(value: T) {
           f(tuple._1(), tuple._2(), tuple._3())
+          mutable.onMutated()
         }
         def unreact() {
         }
@@ -49,6 +52,7 @@ class ReactiveApi {
       val m2 = new Reactor[S] {
         def react(value: S) {
           f(tuple._1(), tuple._2(), tuple._3())
+          mutable.onMutated()
         }
         def unreact() {
         }
@@ -56,6 +60,7 @@ class ReactiveApi {
       val m3 = new Reactor[U] {
         def react(value: U) {
           f(tuple._1(), tuple._2(), tuple._3())
+          mutable.onMutated()
         }
         def unreact() {
         }
