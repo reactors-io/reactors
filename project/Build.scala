bdaf8971b77d2333c3a4693aac40c9104d59d86c
Aleksandar Prokopec <axel22@users.noreply.github.com>
2014-02-20 09:12:36 +0100

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,27 @@ import java.io.File
 
 object ReactressBuild extends Build {
 
+
+  val publishUser = "SONATYPE_USER"
+  
+  val publishPass = "SONATYPE_PASS"
+  
+  val userPass = for {
+    user <- sys.env.get(publishUser)
+    pass <- sys.env.get(publishPass)
+  } yield (user, pass)
+
+  val publishCreds: Seq[Setting[_]] = Seq(userPass match {
+    case Some((user, pass)) =>
+      credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass)
+    case None =>
+      // prevent publishing
+      publish <<= streams.map(_.log.info("Publishing to Sonatype is disabled since the \"" + publishUser + "\" and/or \"" + publishPass + "\" environment variables are not set."))
+  })
+
   val reactressSettings = Defaults.defaultSettings ++ Seq (
+    name := "reactress",
+    version := "0.2-SNAPSHOT",
     organization := "org.reactress",
     scalaVersion := "2.10.2",
     libraryDependencies ++= Seq(
@@ -15,7 +35,42 @@ object ReactressBuild extends Build {
       "com.github.axel22" %% "scalameter" % "0.4" % "test",
       "com.netflix.rxjava" % "rxjava-scala" % "0.15.0" % "test"
     ),
-    testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework")
+    testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
+    parallelExecution in Test := false,
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases"
+    ),
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra :=
+      <url>http://storm-enroute.com/</url>
+      <licenses>
+        <license>
+          <name>BSD-style</name>
+          <url>http://opensource.org/licenses/BSD-3-Clause</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:storm-enroute/reactress.git</url>
+        <connection>scm:git:git@github.com:storm-enroute/reactress.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>axel22</id>
+          <name>Aleksandar Prokopec</name>
+          <url>http://axel22.github.com/</url>
+        </developer>
+      </developers>
   )
 
   lazy val reactress = Project(
