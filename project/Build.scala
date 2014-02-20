0c64ef8788ad3ec1b874b30637c9b7fb41aa9495
Aleksandar Prokopec <axel22@gmail.com>
2014-02-20 09:22:33 +0100

    Promoting to version v0.2

--- project/Build.scala
+++ project/Build.scala
@@ -27,7 +27,7 @@ object ReactressBuild extends Build {
 
   val reactressSettings = Defaults.defaultSettings ++ Seq (
     name := "reactress",
-    version := "0.2-SNAPSHOT",
+    version := "0.2",
     organization := "org.reactress",
     scalaVersion := "2.10.2",
     libraryDependencies ++= Seq(
