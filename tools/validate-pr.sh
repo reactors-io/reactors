0f94477528bd2d1b9bc0f304db470fa38ee20be5
Aleksandar Prokopec <axel22@gmail.com>
2014-02-21 14:18:56 +0100

    Fix travis config.

--- tools/validate-pr.sh
+++ tools/validate-pr.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set +x
+
+TRAVIS_SCALA_VERSION_ARG=$1
+
 if [[ $TRAVIS_PULL_REQUEST == "false" ]];
 then
 	echo "Only building pull requests - build passed."
 	exit 0
 else
-	sbt ++${TRAVIS_SCALA_VERSION} \"testOnly org.reactress.test.*\"
+	sbt $TRAVIS_SCALA_VERSION_ARG 'testOnly org.reactress.test.*'
 fi
