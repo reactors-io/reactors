429fdb42122ff358d7638c2c2a3e6c6ec0f97e04
Aleksandar Prokopec <axel22@gmail.com>
2014-03-14 16:44:51 +0100

    Fixing the implicit classes/specialization interaction issue.

--- src/main/scala/org/reactress/package.scala
+++ src/main/scala/org/reactress/package.scala
@@ -7,7 +7,7 @@ import scala.reflect.ClassTag
 
 
 
-package object reactress extends ReactiveApi {
+package object reactress {
 
   type spec = specialized
 
@@ -171,6 +171,89 @@ package object reactress extends ReactiveApi {
       def newRawArray(sz: Int) = new Array[Int](sz)
     }
   
+  }
+
+  /* extensions on tuples */
+
+  class Tuple2Extensions[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S](val tuple: (Signal[T], Signal[S])) {
+    def mutate[M <: ReactMutable](mutable: M)(f: (T, S) => Unit) = {
+      val s = new Tuple2Extensions.Mutate(tuple, mutable, f)
+      s.subscription = Reactive.CompositeSubscription(
+        tuple._1.onReaction(s.m1),
+        tuple._2.onReaction(s.m2)
+      )
+      s
+    }
+  }
+
+  implicit def tuple2ext[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S](tuple: (Signal[T], Signal[S])) = new Tuple2Extensions[T, S](tuple)
+
+  object Tuple2Extensions {
+    class Mutate[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S, M <: ReactMutable](tuple: (Signal[T], Signal[S]), mutable: M, f: (T, S) => Unit)
+    extends Reactive.ProxySubscription {
+      val m1 = new Reactor[T] {
+        def react(value: T) {
+          f(tuple._1(), tuple._2())
+          mutable.onMutated()
+        }
+        def unreact() {
+        }
+      }
+      val m2 = new Reactor[S] {
+        def react(value: S) {
+          f(tuple._1(), tuple._2())
+          mutable.onMutated()
+        }
+        def unreact() {
+        }
+      }
+      var subscription = Reactive.Subscription.empty
+    }
+  }
+
+  class Tuple3Extensions[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S,  @spec(Int, Long, Double) U](val tuple: (Signal[T], Signal[S], Signal[U])) {
+    def mutate[M <: ReactMutable](mutable: M)(f: (T, S, U) => Unit) = {
+      val s = new Tuple3Extensions.Mutate(tuple, mutable, f)
+      s.subscription = Reactive.CompositeSubscription(
+        tuple._1.onReaction(s.m1),
+        tuple._2.onReaction(s.m2),
+        tuple._3.onReaction(s.m3)
+      )
+      s
+    }
+  }
+
+  implicit def tuple3ext[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S,  @spec(Int, Long, Double) U](tuple: (Signal[T], Signal[S], Signal[U])) = new Tuple3Extensions[T, S, U](tuple)
+
+  object Tuple3Extensions {
+    class Mutate[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S, @spec(Int, Long, Double) U, M <: ReactMutable](tuple: (Signal[T], Signal[S], Signal[U]), mutable: M, f: (T, S, U) => Unit)
+    extends Reactive.ProxySubscription {
+      val m1 = new Reactor[T] {
+        def react(value: T) {
+          f(tuple._1(), tuple._2(), tuple._3())
+          mutable.onMutated()
+        }
+        def unreact() {
+        }
+      }
+      val m2 = new Reactor[S] {
+        def react(value: S) {
+          f(tuple._1(), tuple._2(), tuple._3())
+          mutable.onMutated()
+        }
+        def unreact() {
+        }
+      }
+      val m3 = new Reactor[U] {
+        def react(value: U) {
+          f(tuple._1(), tuple._2(), tuple._3())
+          mutable.onMutated()
+        }
+        def unreact() {
+        }
+      }
+      var subscription = Reactive.Subscription.empty
+    }
   }
 
 }
