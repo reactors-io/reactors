f8d74811e325e8e12f2d17edc44f3d91d1e9c46e
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2014-05-07 16:35:00 +0200

    Basic isolate docs.

--- src/main/scala/org/reactress/Isolate.scala
+++ src/main/scala/org/reactress/Isolate.scala
@@ -11,6 +11,57 @@ import isolate._
 /** An isolated, independent event propagation context.
  *
  *  An `Isolate[T]` object accepts events of type `T` on its input channel.
+ *  One isolate can propagate events concurrently to other isolates --
+ *  it is a basic element of concurrency.
+ *  Reactive values cannot be shared between isolates --
+ *  it is an error to use a reactive value originating in one isolate
+ *  in some different isolate.
+ *
+ *  Isolates are defined by extending the `Isolate` trait.
+ *  The events passed to isolates can be subscribed to using
+ *  their `source` reactive.
+ *  Here is an example:
+ *
+ *  {{{
+ *  class MyPrinter extends Isolate[String] {
+ *    react <<= source onEvent {
+ *      e => println(e)
+ *    }
+ *  }
+ *  }}}
+ *
+ *  Separate isolate instances that exist at runtime
+ *  are created using isolate systems.
+ *  The `isolate` method in the isolate system requires a scheduler
+ *  to execute the isolate.
+ *  Here is an example:
+ *
+ *  {{{
+ *  import Scheduler.Implicits.globalExecutionContext
+ *  val isolateSystem = IsolateSystem.default("MyIsolateSystem")
+ *  val channel = isolateSystem.isolate(Proto[MyPrinter])
+ *  }}}
+ *
+ *  Creating an isolate returns its channel.
+ *  Reactives can be attached to channels to propagate their events to isolates.
+ *
+ *  {{{
+ *  val emitter = new Reactive.Emitter[String]
+ *  channel.attach(emitter)
+ *  emitter += "Hi!" // eventually, this is printed by `MyPrinter`
+ *  }}}
+ *
+ *  To stop an isolate, its channel needs to be sealed, 
+ *  and all the previously attached reactives need to be closed.
+ *
+ *  {{{
+ *  emitter.close()
+ *  channel.seal()
+ *  }}}
+ *
+ *  Isolates also receive special `SysEvent`s on the `sysEvents` reactive.
+ *  If a subscription on the `source` reactive throws a non-fatal exception,
+ *  the exception is emitted on the `failures` reactive.
  *  
  *  @tparam T        the type of the events this isolate produces
  */
