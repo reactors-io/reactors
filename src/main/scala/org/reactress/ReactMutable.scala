5dcc0700fd9e02328277afe67ab92f35b0e91dbb
Aleksandar Prokopec <axel22@users.noreply.github.com>
2014-05-09 12:52:55 +0200

    Update ReactMutable.scala

--- src/main/scala/org/reactress/ReactMutable.scala
+++ src/main/scala/org/reactress/ReactMutable.scala
@@ -6,17 +6,27 @@ import collection._
 
 
 
+/** Describes reactives that are either mutable, or contain and emit events that are themselves mutable.
+ */
 trait ReactMutable {
 
+  /** Called internally - binds a subscription to this reactive mutable value.
+   */
   def bindSubscription(s: Reactive.Subscription): Reactive.Subscription = s
 
+  /** Called internally - when the mutable reactive or its internal value has been mutated.
+   */
   def onMutated(): Unit = {}
 
 }
 
 
+/** Helper traits and implementations for reactive mutables.
+ */
 object ReactMutable {
 
+  /** Used internally - holds a list of subscriptions bound to this reactive mutable.
+   */
   trait Subscriptions extends ReactMutable {
     val subscriptions = mutable.Set[Reactive.Subscription]()
 
