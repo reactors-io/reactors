43834d86cab72472248dc43f2d17e6c15ee59cac
U-phoenix\axel22 <axel22@gmail.com>
2014-06-04 22:56:52 +0200

    Change reactive random generators.

--- src/main/scala/scala/reactive/calc/ReactRandom.scala
+++ src/main/scala/scala/reactive/calc/ReactRandom.scala
@@ -9,23 +9,41 @@ import scala.util.Random
 
 /** Reactive random value.
  *
- *  It can be used as a regular `Random` object to obtain random values:
+ *  It can be used as a regular random generator that produces random values:
  *
  *  {{{
  *  val r = new ReactRandom(1L)
- *  r.nextDouble()
+ *  r.double()
  *  }}}
  *
  *  Or, as a reactive random value, by subscribing to events produced when `emit` is called:
  *
  *  {{{
- *  val evens = r.react.ints.filter(_ % 2 == 0)
+ *  val evens = r.react.int().filter(_ % 2 == 0)
  *  }}}
  *
  *  Above, `evens` contains random values.
  */
-class ReactRandom(private val jucRandom: java.util.Random) extends Random(jucRandom) {
+class ReactRandom(private val jucRandom: java.util.Random) {
   self =>
+
+  private val rand = new Random(jucRandom)
+
+  def boolean() = rand.nextBoolean()
+
+  def int() = rand.nextInt()
+
+  def int(n: Int) = rand.nextInt(n)
+
+  def float() = rand.nextFloat()
+
+  def long() = rand.nextLong()
+
+  def long(n: Int) = math.abs(rand.nextLong()) % n
+
+  def double() = rand.nextDouble()
+
+  def string(length: Int) = rand.nextString(length)
 
   def this(seed: Long) = this(new java.util.Random(seed))
 
@@ -35,9 +53,12 @@ class ReactRandom(private val jucRandom: java.util.Random) extends Random(jucRan
 
   object react {
     private[calc] val randomEmitter = new Reactive.SideEffectEmitter[Unit](() => ())
-    def ints = randomEmitter.map(_ => self.nextInt())
-    def longs = randomEmitter.map(_ => self.nextLong())
-    def doubles = randomEmitter.map(_ => self.nextDouble())
+    def int() = randomEmitter.map(_ => self.int())
+    def int(n: Int) = randomEmitter.map(_ => self.int(n))
+    def long() = randomEmitter.map(_ => self.long())
+    def long(n: Int) = randomEmitter.map(_ => self.long(n))
+    def double() = randomEmitter.map(_ => self.double())
+    def string(length: Int) = randomEmitter.map(_ => self.string(length))
   }
 
 }
