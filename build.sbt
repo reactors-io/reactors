a20f3ed62e9bdfc7986197058adbed093d31ee57
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2014-08-05 19:57:46 +0200

    Move credentials up.

---
+++ build.sbt
@@ -0,0 +1,26 @@
+
+
+
+// in newer SBT versions, this apparently has to go to `build.sbt`
+
+{
+  val publishUser = "SONATYPE_USER"
+  val publishPass = "SONATYPE_PASS"
+  val userPass = for {
+    user <- sys.env.get(publishUser)
+    pass <- sys.env.get(publishPass)
+  } yield (user, pass)
+  val publishCreds: Seq[Setting[_]] = Seq(userPass match {
+    case Some((user, pass)) =>
+      println(s"Username and password for Sonatype picked up: '$user', '${pass.head}...${pass.last}'")
+      credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass)
+    case None =>
+      // prevent publishing
+      val errorMessage =
+        "Publishing to Sonatype is disabled since the \"" +
+        publishUser + "\" and/or \"" + publishPass + "\" environment variables are not set."
+      println(errorMessage)
+      publish <<= streams.map(_.log.info(errorMessage))
+  })
+  publishCreds
+}
--- project/Build.scala
+++ project/Build.scala
@@ -7,26 +7,9 @@ import java.io.File
 
 object ReactiveCollectionsBuild extends Build {
 
-  val publishUser = "SONATYPE_USER"
-  
-  val publishPass = "SONATYPE_PASS"
-
   val frameworkVersion = "0.5"
-  
-  val userPass = for {
-    user <- sys.env.get(publishUser)
-    pass <- sys.env.get(publishPass)
-  } yield (user, pass)
 
-  val publishCreds: Seq[Setting[_]] = Seq(userPass match {
-    case Some((user, pass)) =>
-      credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass)
-    case None =>
-      // prevent publishing
-      publish <<= streams.map(_.log.info("Publishing to Sonatype is disabled since the \"" + publishUser + "\" and/or \"" + publishPass + "\" environment variables are not set."))
-  })
-
-  val reactiveCollectionsSettings = Defaults.defaultSettings ++ publishCreds ++ Seq (
+  val reactiveCollectionsSettings = Defaults.defaultSettings ++ Seq (
     name := "reactive-collections",
     version := frameworkVersion,
     organization := "com.storm-enroute",
@@ -90,7 +73,7 @@ object ReactiveCollectionsBuild extends Build {
     case _ => Nil
   }
 
-  val reactiveCollectionsCoreSettings = Defaults.defaultSettings ++ publishCreds ++ Seq (
+  val reactiveCollectionsCoreSettings = Defaults.defaultSettings ++ Seq (
     name := "reactive-collections-core",
     version := frameworkVersion,
     organization := "com.storm-enroute",
