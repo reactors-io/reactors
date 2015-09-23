e8c877c9f7791236738755baed9cf30682d4f35a
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2015-09-23 15:31:09 +0200

    WIP on the remoting system.

--- src/main/scala/scala/reactive/remoting/Remoting.scala
+++ src/main/scala/scala/reactive/remoting/Remoting.scala
@@ -4,9 +4,13 @@ package remoting
 
 
 import io.netty.bootstrap._
+import io.netty.buffer._
+import scala.collection._
 
 
 
 class Remoting(val system: IsoSystem) extends Protocol {
+  private val connections = mutable.Map[SystemUrl, AnyRef]()
+
   def resolve[T](channelUrl: ChannelUrl): Channel[T] = ???
 }
