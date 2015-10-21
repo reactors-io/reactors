94f290cc07a006e09a65aaa6c44fe03c70d5a820
axel22 <aleksandar.prokopec@gmail.com>
2015-10-21 18:05:31 +0200

    Add stub for `UdpChannel`s.

--- src/main/scala/scala/reactive/remoting/Remoting.scala
+++ src/main/scala/scala/reactive/remoting/Remoting.scala
@@ -11,7 +11,16 @@ import scala.reactive.core.UnrolledRing
 
 
 class Remoting(val system: IsoSystem) extends Protocol {
+  private class UdpChannel[T](url: ChannelUrl) extends Channel[T] {
+    def !(x: T): Unit = ???
+
+    def isSealed: Boolean = ???
+  }
+
   def resolve[T](channelUrl: ChannelUrl): Channel[T] = {
-    ???
+    channelUrl.isoUrl.systemUrl.schema match {
+      case "rc.udp" => new UdpChannel(url)
+      case s => sys.error("Unknown channel schema: $s")
+    }
   }
 }
