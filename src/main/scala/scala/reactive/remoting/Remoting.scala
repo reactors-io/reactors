22a201cfbe20cfc58ace2db9ddcacf9ae2a5e889
axel22 <aleksandar.prokopec@gmail.com>
2015-11-16 23:16:53 +0100

    Add `depickle` method to `Remoting.Pickler`.

--- src/main/scala/scala/reactive/remoting/Remoting.scala
+++ src/main/scala/scala/reactive/remoting/Remoting.scala
@@ -3,8 +3,7 @@ package remoting
 
 
 
-import java.io.OutputStream
-import java.io.ObjectOutputStream
+import java.io._
 import java.net._
 import java.nio.ByteBuffer
 import java.nio.channels.DatagramChannel
@@ -166,6 +165,7 @@ object Remoting {
    */
   trait Pickler {
     def pickle[@spec(Int, Long, Double) T](x: T, buffer: ByteBuffer): Unit
+    def depickle[@spec(Int, Long, Double) T](buffer: ByteBuffer): T
   }
 
   object Pickler {
@@ -173,9 +173,14 @@ object Remoting {
      */
     class JavaSerialization extends Pickler {
       def pickle[@spec(Int, Long, Double) T](x: T, buffer: ByteBuffer) = {
-        val outputStream = new ByteBufferOutputStream(buffer)
-        val objectOutputStream = new ObjectOutputStream(outputStream)
-        objectOutputStream.writeObject(x)
+        val os = new ByteBufferOutputStream(buffer)
+        val oos = new ObjectOutputStream(os)
+        oos.writeObject(x)
+      }
+      def depickle[@spec(Int, Long, Double) T](buffer: ByteBuffer): T = {
+        val is = new ByteBufferInputStream(buffer)
+        val ois = new ObjectInputStream(is)
+        ois.readObject().asInstanceOf[T]
       }
     }
   }
@@ -184,6 +189,18 @@ object Remoting {
     def write(b: Int): Unit = buf.put(b.toByte)
     override def write(bytes: Array[Byte], off: Int, len: Int): Unit = {
       buf.put(bytes, off, len)
+    }
+  }
+
+  private class ByteBufferInputStream(val buffer: ByteBuffer) extends InputStream {
+    def read() = buffer.get()
+    override def read(dst: Array[Byte], offset: Int, length: Int) = {
+      val count = math.min(buffer.remaining, length)
+      if (count == 0) -1
+      else {
+        buffer.get(dst, offset, length)
+        count
+      }
     }
   }
 
