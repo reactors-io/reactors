6e7d7667adb1ee8da1508fd3951c21c76db560f8
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2016-02-20 21:56:28 +0100

    Add `count` combinator for containers.

--- reactors-container/src/main/scala/org/reactors/container/RContainer.scala
+++ reactors-container/src/main/scala/org/reactors/container/RContainer.scala
@@ -13,30 +13,28 @@ trait RContainer[@spec(Int, Long, Double) T] {
   
   def removes: Events[T]
 
-  def react: RContainer.Lifted[T]
-
   def foreach: Events[T]
 
   def size: Events[Int]
 
-  def count(p: T => Boolean): Events[Int]
+  def count(p: T => Boolean): Events[Int] = new RContainer.Count(this, p)
 
-  def forall(p: T => Boolean): Events[Boolean]
+  // def forall(p: T => Boolean): Events[Boolean]
 
-  def exists(p: T => Boolean): Events[Boolean]
+  // def exists(p: T => Boolean): Events[Boolean]
 
-  def toAggregate(z: T)(op: (T, T) => T): Signal[T]
+  // def toAggregate(z: T)(op: (T, T) => T): Signal[T]
 
-  def toCommutativeAggregate(z: T)(op: (T, T) => T): Signal[T]
+  // def toCommutativeAggregate(z: T)(op: (T, T) => T): Signal[T]
 
-  def toAbelianAggregate(z: T)(op: (T, T) => T)(inv: (T, T) => T): Signal[T]
+  // def toAbelianAggregate(z: T)(op: (T, T) => T)(inv: (T, T) => T): Signal[T]
 
-  def map[@spec(Int, Long, Double) S](f: T => S): RContainer[S]
+  // def map[@spec(Int, Long, Double) S](f: T => S): RContainer[S]
 
-  def filter(p: T => Boolean): RContainer[T]
+  // def filter(p: T => Boolean): RContainer[T]
 
-  def collect[S <: AnyRef](pf: PartialFunction[T, S])(implicit e: T <:< AnyRef):
-    RContainer[S]
+  // def collect[S <: AnyRef](pf: PartialFunction[T, S])(implicit e: T <:< AnyRef):
+  //   RContainer[S]
 
   // def union(that: RContainer[T])(
   //   implicit count: RContainer.Union.Count[T], a: Arrayable[T]
@@ -49,285 +47,60 @@ trait RContainer[@spec(Int, Long, Double) T] {
 
 object RContainer {
 
-  trait Lifted[@spec(Int, Long, Double) T] {
-    val container: RContainer[T]
-
-    /* queries */
-
-    // def size: Signal[Int] with Events.Subscription =
-    //   new Size(container)
-
-    // def count(p: T => Boolean): Signal[Int] with Events.Subscription =
-    //   new Signal.Default[Int] with Events.ProxySubscription {
-    //     private[reactive] var value = container.count(p)
-    //     def apply() = value
-    //     val subscription = Events.CompositeSubscription(
-    //       container.inserts foreach { x =>
-    //         if (p(x)) { value += 1; reactAll(value) }
-    //       },
-    //       container.removes foreach { x =>
-    //         if (p(x)) { value -= 1; reactAll(value) }
-    //       }
-    //     )
-    //   }
-
-    // def exists(p: T => Boolean): Signal[Boolean] with Events.Subscription =
-    //   count(p).map(_ > 0)
-
-    // def forall(p: T => Boolean): Signal[Boolean] with Events.Subscription =
-    //   new Signal.Default[Boolean] with Events.ProxySubscription {
-    //     private[reactive] var value = container.count(p)
-    //     def apply() = value == container.size
-    //     val subscription = Events.CompositeSubscription(
-    //       container.inserts foreach { x =>
-    //         if (p(x)) value += 1; reactAll(value == container.size)
-    //       },
-    //       container.removes foreach { x =>
-    //         if (p(x)) value -= 1; reactAll(value == container.size)
-    //       }
-    //     )
-    //   }
-
-    // def foreach(f: T => Unit): Events.Subscription = {
-    //   container.foreach(f)
-    //   container.inserts.foreach(f)
-    // }
-    
-    // def monoidFold(implicit m: Monoid[T]): Signal[T] with Events.Subscription =
-    //   new Aggregate(container, MonoidCatamorph[T])
-
-    // def commuteFold(implicit c: Commutoid[T]): Signal[T] with Events.Subscription =
-    //   new Aggregate(container, CommuteCatamorph[T])
-
-    // def abelianFold(implicit ab: Abelian[T], a: Arrayable[T]):
-    //   Signal[T] with Events.Subscription =
-    //   new Aggregate(container, AbelianCatamorph[T])
-
-    // def mutate(m: ReactMutable)(insert: T => Unit)(remove: T => Unit):
-    //   Events.Subscription =
-    //   new Mutate(container, insert, remove)
-
-    // /* transformers */
-  
-    // def to[That <: RContainer[T]](implicit factory: RBuilder.Factory[T, That]): That = {
-    //   val builder = factory()
-    //   val result = builder.container
-  
-    //   result.subscriptions += container.inserts.mutate(builder) {
-    //     builder += _
-    //   }
-    //   result.subscriptions += container.removes.mutate(builder) {
-    //     builder -= _
-    //   }
-  
-    //   result
-    // }
-
+  class Count[@spec(Int, Long, Double) T](
+    val self: RContainer[T],
+    val pred: T => Boolean
+  ) extends Events[Int] {
+    def onReaction(obs: Observer[Int]): Subscription = {
+      var initial = 0
+      self.foreach.onEvent(x => if (pred(x)) initial += 1).unsubscribe()
+      val insertObs = new CountInsertObserver(obs, initial, pred)
+      val removeObs = new CountRemoveObserver(obs, insertObs, pred)
+      new Subscription.Composite(
+        self.inserts.onReaction(insertObs),
+        self.inserts.onReaction(removeObs)
+      )
+    }
   }
 
-  object Lifted {
-    // class Default[@spec(Int, Long, Double) T](val container: RContainer[T])
-    // extends Lifted[T]
+  class CountInsertObserver[@spec(Int, Long, Double) T](
+    val target: Observer[Int],
+    var count: Int,
+    val pred: T => Boolean
+  ) extends Observer[T] {
+    var done = false
+    def init(self: Observer[T]) {
+      target.react(count)
+    }
+    init(this)
+    def react(x: T) = if (!done) {
+      if (pred(x)) {
+        count += 1
+        target.react(count)
+      }
+    }
+    def except(t: Throwable) = if (!done) {
+      target.except(t)
+    }
+    def unreact() = if (!done) {
+      done = true
+      target.unreact()
+    }
   }
 
-  // trait Default[@spec(Int, Long, Double) T] extends RContainer[T] {
-  //   val react = new Lifted.Default[T](this)
-  // }
-
-  // class Size[@spec(Int, Long, Double) T](self: RContainer[T])
-  // extends Signal.Default[Int] with Events.ProxySubscription {
-  //   private[reactive] var value = self.size
-  //   def apply() = value
-  //   val subscription = Events.CompositeSubscription(
-  //     self.inserts foreach { _ => value += 1; reactAll(value) },
-  //     self.removes foreach { _ => value -= 1; reactAll(value) }
-  //   )
-  // }
-
-  // class Mutate[@spec(Int, Long, Double) T]
-  //   (self: RContainer[T], insert: T => Unit, remove: T => Unit)
-  // extends Events.ProxySubscription {
-  //   val subscription = Events.CompositeSubscription(
-  //     self.inserts.mutate(self)(insert),
-  //     self.removes.mutate(self)(remove)
-  //   )
-  // }
-
-  // class Map[@spec(Int, Long, Double) T, @spec(Int, Long, Double) S]
-  //   (self: RContainer[T], f: T => S)
-  // extends RContainer.Default[S] {
-  //   val inserts = self.inserts.map(f)
-  //   val removes = self.removes.map(f)
-  //   def size = self.size
-  //   def foreach(g: S => Unit) = self.foreach(x => g(f(x)))
-  // }
-
-  // class Filter[@spec(Int, Long, Double) T](self: RContainer[T], p: T => Boolean)
-  // extends RContainer.Default[T] {
-  //   val inserts = self.inserts.filter(p)
-  //   val removes = self.removes.filter(p)
-  //   def size = self.count(p)
-  //   def foreach(f: T => Unit) = self.foreach(x => if (p(x)) f(x))
-  // }
-
-  // class Collect[T, S <: AnyRef]
-  //   (self: RContainer[T], pf: PartialFunction[T, S])
-  //   (implicit e: T <:< AnyRef)
-  // extends RContainer.Default[S] {
-  //   val inserts = self.inserts.collect(pf)
-  //   val removes = self.removes.collect(pf)
-  //   def size = self.count(pf.isDefinedAt)
-  //   def foreach(f: S => Unit) =
-  //     self.foreach(x => if (pf.isDefinedAt(x)) f(pf(x)))
-  // }
-
-  // class Union[@spec(Int, Long, Double) T]
-  //   (self: RContainer[T], that: RContainer[T], count: Union.Count[T])
-  //   (implicit at: Arrayable[T])
-  // extends RContainer.Default[T] {
-  //   val inserts = new Events.BindEmitter[T]
-  //   val removes = new Events.BindEmitter[T]
-  //   var countSignal: Signal.Mutable[Union.Count[T]] = new Signal.Mutable(count)
-  //   var insertSubscription =
-  //     (self.inserts union that.inserts).mutate(countSignal, inserts) { x =>
-  //       if (count.inc(x)) inserts.react(x)
-  //     }
-  //   var removeSubscription =
-  //     (self.removes union that.removes).mutate(countSignal, removes) { x =>
-  //       if (count.dec(x)) removes.react(x)
-  //     }
-  //   def computeUnion = {
-  //     val s = RHashSet[T]
-  //     for (v <- self) s += v
-  //     for (v <- that) s += v
-  //     s
-  //   }
-  //   def size = computeUnion.size
-  //   def foreach(f: T => Unit) = computeUnion.foreach(f)
-  // }
-
-  // object Union {
-  //   trait Count[@spec(Int, Long, Double) T] {
-  //     def inc(x: T): Boolean
-  //     def dec(x: T): Boolean
-  //   }
-
-  //   class PrimitiveCount
-  //     [@spec(Int, Long, Double) T](implicit val at: Arrayable[T])
-  //   extends Count[T] {
-  //     val table = RHashValMap[T, Int](at, Arrayable.nonZeroInt)
-  //     def inc(x: T) = {
-  //       val curr = table.applyOrNil(x)
-  //       table(x) = curr + 1
-  //       if (curr == 0) true
-  //       else false
-  //     }
-  //     def dec(x: T) = {
-  //       val curr = table.applyOrNil(x)
-  //       if (curr <= 1) {
-  //         table.remove(x)
-  //         true
-  //       } else {
-  //         table(x) = curr - 1
-  //         false
-  //       }
-  //     }
-  //   }
-
-  //   sealed trait Numeral {
-  //     def inc: Numeral
-  //     def dec: Numeral
-  //   }
-  //   object Zero extends Numeral {
-  //     def inc = One
-  //     def dec = Zero
-  //   }
-  //   object One extends Numeral {
-  //     def inc = Two
-  //     def dec = Zero
-  //   }
-  //   object Two extends Numeral {
-  //     def inc = Two
-  //     def dec = One
-  //   }
-
-  //   class RefCount[T](implicit val at: Arrayable[T]) extends Count[T] {
-  //     val table = RHashValMap[T, Numeral]
-  //     def inc(x: T) = {
-  //       val curr = table.applyOrElse(x, Zero)
-  //       table(x) = curr.inc
-  //       if (curr == Zero) true
-  //       else false
-  //     }
-  //     def dec(x: T) = {
-  //       val curr = table.applyOrElse(x, Zero)
-  //       val next = curr.dec
-  //       if (next == Zero) {
-  //         table.remove(x)
-  //         true
-  //       } else {
-  //         table(x) = next
-  //         false
-  //       }
-  //     }
-  //   }
-
-  //   implicit def intCount = new PrimitiveCount[Int]
-
-  //   implicit def longCount = new PrimitiveCount[Long]
-
-  //   implicit def doubleCount = new PrimitiveCount[Double]
-
-  //   implicit def refCount[T >: Null <: AnyRef](implicit at: Arrayable[T]) =
-  //     new RefCount[T]
-
-  // }
-
-  // class Aggregate[@spec(Int, Long, Double) T]
-  //   (val container: RContainer[T], val catamorph: RCatamorph[T, T])
-  // extends Signal.Proxy[T] with Events.ProxySubscription {
-  //   commuted =>
-  //   val id = (v: T) => v
-  //   var subscription: Events.Subscription = _
-  //   var proxy: Signal[T] = _
-
-  //   def init(c: RContainer[T]) {
-  //     proxy = catamorph.signal
-  //     for (v <- container) catamorph += v
-  //     subscription = Events.CompositeSubscription(
-  //       container.inserts foreach { v => catamorph += v },
-  //       container.removes foreach { v => catamorph -= v }
-  //     )
-  //   }
-
-  //   init(container)
-  // }
-
-  // /* default containers */
-
-  // class Emitter[@spec(Int, Long, Double) T]
-  //   (private val foreachF: (T => Unit) => Unit, private val sizeF: () => Int)
-  // extends RContainer[T] {
-  //   private[reactive] var insertsEmitter: Events.Emitter[T] = null
-  //   private[reactive] var removesEmitter: Events.Emitter[T] = null
-
-  //   private def init(dummy: RContainer.Emitter[T]) {
-  //     insertsEmitter = new Events.Emitter[T]
-  //     removesEmitter = new Events.Emitter[T]
-  //   }
-
-  //   init(this)
-
-  //   def inserts = insertsEmitter
-
-  //   def removes = removesEmitter
-
-  //   def foreach(f: T => Unit) = foreachF(f)
-
-  //   def size = sizeF()
-
-  //   def react = new RContainer.Lifted.Default(this)
-
-  // }
+  class CountRemoveObserver[@spec(Int, Long, Double) T](
+    val target: Observer[Int],
+    val insertObs: CountInsertObserver[T],
+    val pred: T => Boolean
+  ) extends Observer[T] {
+    def react(x: T) = if (!insertObs.done) {
+      if (pred(x)) {
+        insertObs.count -= 1
+        target.react(insertObs.count)
+      }
+    }
+    def except(t: Throwable) = insertObs.except(t)
+    def unreact() = insertObs.unreact()
+  }
 
 }
