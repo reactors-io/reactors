d729abc0295426735ba90948c1ba72365ac81e8c
axel22 <aleksandar.prokopec@gmail.com>
2016-05-03 21:23:51 +0200

    Add unit test for balancers.

--- reactors-protocols/src/test/scala/io/reactors/protocol/balancer-tests.scala
+++ reactors-protocols/src/test/scala/io/reactors/protocol/balancer-tests.scala
@@ -18,8 +18,21 @@ import scala.concurrent.duration._
 class BalancerProtocolsSpec extends FunSuite {
   val system = ReactorSystem.default("balancer-protocols")
 
-  test("default balancer") {
-
+  test("default balancer with single channel") {
+    val done = Promise[Boolean]()
+    system.spawn(Reactor[Int] { self =>
+      val bc = system.channels.daemon.balancer(Seq(self.main.channel))
+      bc.channel ! 17
+      self.main.events onEvent { x =>
+        if (x == 17) {
+          done.success(true)
+          self.main.seal()
+        } else {
+          done.success(false)
+        }
+      }
+    })
+    assert(Await.result(done.future, 10.seconds))
   }
 }
 
@@ -28,10 +41,26 @@ class BalancerProtocolsCheck
 extends Properties("ServerProtocols") with ExtendedProperties {
   val system = ReactorSystem.default("check-system")
 
-  val sizes = detChoose(0, 256)
+  val sizes = detChoose(0, 128)
 
   property("round-robin balancer") = forAllNoShrink(sizes) { num =>
     stackTraced {
+      val ps = for (i <- 0 until num) yield Promise[Int]()
+      val done = Promise[Boolean]()
+      val chs = for (i <- 0 until num) yield system.spawn(Reactor[Int] { self =>
+        self.main.events onEvent { x =>
+          ps(i).success(x)
+          self.main.seal()
+        }
+      })
+      val balancer = system.spawn(Reactor[Int] { self =>
+        val bc = system.channels.daemon.balancer(chs)
+        for (i <- 0 until num) bc.channel ! i
+        self.main.seal()
+        done.success(true)
+      })
+      assert(Await.result(done.future, 10.seconds))
+      for (i <- 0 until num) assert(Await.result(ps(i).future, 10.seconds) == i)
       true
     }
   }
