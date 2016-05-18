89a9ecc9ba3fe9fd2901befc59d312ac3f33cd68
axel22 <aleksandar.prokopec@gmail.com>
2016-05-18 09:51:28 +0200

    Add stub for the reactor section.

---
+++ reactors-core/src/test/scala/tutorial/reactors.scala
@@ -0,0 +1,20 @@
+package tutorial
+
+
+
+import io.reactors._
+import org.scalatest._
+
+
+
+class Reactors extends FunSuite with Matchers {
+  /*!md
+  ## Reactors
+
+  As we learned previously, event streams always propagate events on a single thread.
+  
+  !*/
+
+  /*!begin-code!*/
+  /*!end-code!*/
+}
