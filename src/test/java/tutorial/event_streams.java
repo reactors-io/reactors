8955059f30076816bdfe3fac8933523b41421aa8
axel22 <aleksandar.prokopec@gmail.com>
2016-06-25 09:15:38 +0200

    Create stub for event stream example tests.

---
+++ src/test/java/tutorial/event_streams.java
@@ -0,0 +1,16 @@
+package tutorial;
+
+
+
+import io.reactors.japi.*;
+import java.util.concurrent.*;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class event_streams {
+
+}
