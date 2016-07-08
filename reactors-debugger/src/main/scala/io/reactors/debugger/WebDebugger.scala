a02c71771302e54b844db8df8de204748a92ae34
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2016-07-08 17:37:52 +0200

    Use topo-sort for page expansion.

--- reactors-debugger/src/main/scala/io/reactors/debugger/WebDebugger.scala
+++ reactors-debugger/src/main/scala/io/reactors/debugger/WebDebugger.scala
@@ -60,101 +60,119 @@ object WebDebugger {
     val port = system.bundle.config.getInt("debug-api.port")
 
     def loadPage(path: String): String = {
-      val libs = mutable.Map[String, String]()
-      val styles = mutable.Map[String, String]()
-      val components = mutable.Map[String, String]()
-      val libOrder = mutable.Buffer[String]()
-      val styleOrder = mutable.Buffer[String]()
-      val componentOrder = mutable.Buffer[String]()
+      sealed trait NodeType
+      case object Com extends NodeType
+      case object Lib extends NodeType
+      case object Style extends NodeType
+      case object Page extends NodeType
+
+      class Node(val path: String, val nodeType: NodeType, var content: String = "") {
+        val deps = mutable.LinkedHashMap[String, Node]()
+      }
+
       val libPattern = Pattern.compile("\\s*@@library\\((?<path>.*)\\)")
       val stylePattern = Pattern.compile("\\s*@@style\\((?<path>.*)\\)")
       val componentPattern = Pattern.compile("\\s*@@component\\((?<path>.*)\\)")
+      val seen = mutable.Map[String, Node]()
 
-      def interpolate(raw: String): String = {
+      def add(n: Node): Node = {
+        seen(n.path) = n
+        n
+      }
+
+      def interpolate(n: Node): String = {
         val scopes = new HashMap[String, Object]
         scopes.put("url", s"${system.bundle.urlMap("reactor.udp").url.host}:$port")
         val imports = {
           val sb = new StringBuffer
-          def insert(
-            before: String, after: String,
-            chunks: mutable.Map[String, String], order: Seq[String]
-          ) {
-            for (path <- order.reverse) {
-              if (chunks.contains(path)) {
-                sb.append(before)
-                sb.append("\n")
-                sb.append(chunks(path))
-                sb.append("\n")
-                sb.append(after)
-                sb.append("\n\n")
-                chunks.remove(path)
-              }
+          def traverse(n: Node): Unit = if (seen.contains(n.path)) {
+            seen.remove(n.path)
+            for ((path, d) <- n.deps) {
+              traverse(d)
             }
+            sb.append("\n\n")
+            n.nodeType match {
+              case Style =>
+                sb.append("<style>\n")
+                sb.append(n.content)
+                sb.append("\n</style>\n")
+              case Lib =>
+                sb.append("<script type='text/javascript'>\n")
+                sb.append(n.content)
+                sb.append("\n</script>\n")
+              case Com =>
+                sb.append(n.content)
+              case Page =>
+            }
+            sb.append("\n\n")
           }
-          insert("<script type='text/javascript'>", "</script>", libs, libOrder)
-          insert("<style>", "</style>", styles, styleOrder)
-          insert("", "", components, componentOrder)
+          traverse(n)
           sb.toString
         }
         scopes.put("imports", imports)
 
         val writer = new StringWriter
         val mf = new DefaultMustacheFactory()
-        val mustache = mf.compile(new StringReader(raw), s"${system.name}.template")
+        val mustache =
+          mf.compile(new StringReader(n.content), s"${system.name}.template")
         mustache.execute(writer, scopes)
         writer.toString
       }
 
-      def loadPage(path: String): String = {
+      def loadPage(path: String): Node = {
         def loadString(path: String) = {
           val stream = getClass.getResourceAsStream("/" + path)
           if (stream == null) sys.error(s"Cannot find path: $path")
           IOUtils.toString(stream, "UTF-8")
         }
 
-        def loadLibrary(path: String) = loadString(path)
+        def loadLibrary(path: String): Node =
+          if (seen.contains(path)) seen(path)
+          else add(new Node(path, Lib, loadString(path)))
 
-        def loadStyle(path: String): String = loadString(path)
+        def loadStyle(path: String): Node =
+          if (seen.contains(path)) seen(path)
+          else add(new Node(path, Style, loadString(path)))
 
-        def loadComponent(path: String): String = {
-          val text = loadString(path)
-          val sb = new StringBuffer
-          val reader = new BufferedReader(new StringReader(text))
-          var line: String = null
-          while ({ line = reader.readLine(); line != null }) {
-            def path(p: Pattern, txt: String): String = {
-              val m = p.matcher(txt)
-              if (m.matches()) m.group("path") else null
+        def loadCom(path: String, t: NodeType = Com): Node =
+          if (seen.contains(path)) seen(path) else {
+            val com = add(new Node(path, t))
+            val raw = loadString(path)
+            val reader = new BufferedReader(new StringReader(raw))
+            val sb = new StringBuffer
+
+            var line: String = null
+            while ({ line = reader.readLine(); line != null }) {
+              def path(p: Pattern, txt: String): String = {
+                val m = p.matcher(txt)
+                if (m.matches()) m.group("path") else null
+              }
+
+              val stylepath = path(stylePattern, line)
+              if (stylepath != null) {
+                com.deps(stylepath) = loadStyle(stylepath)
+              }
+
+              val libpath = path(libPattern, line)
+              if (libpath != null) {
+                com.deps(libpath) = loadLibrary(libpath)
+              }
+
+              val compath = path(componentPattern, line)
+              if (compath != null) {
+                com.deps(compath) = loadCom(compath)
+              }
+
+              if (libpath == null && stylepath == null && compath == null) {
+                sb.append(line).append("\n")
+              }
             }
+            com.content = sb.toString
 
-            val stylepath = path(stylePattern, line)
-            if (stylepath != null) {
-              styleOrder += stylepath
-              if (!styles.contains(stylepath)) styles(stylepath) = loadStyle(stylepath)
-            }
-
-            val libpath = path(libPattern, line)
-            if (libpath != null) {
-              libOrder += libpath
-              if (!libs.contains(libpath)) libs(libpath) = loadLibrary(libpath)
-            }
-
-            val compath = path(componentPattern, line)
-            if (compath != null) {
-              componentOrder += compath
-              if (!components.contains(compath))
-                components(compath) = loadComponent(compath)
-            }
-
-            if (libpath == null && stylepath == null && compath == null) {
-              sb.append(line).append("\n")
-            }
+            com
           }
-          sb.toString
-        }
 
-        val expanded = loadComponent(path)
-        expanded
+        loadCom(path, Page)
       }
 
       interpolate(loadPage(path))
