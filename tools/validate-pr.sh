1ca3636234d2e54ab289319ba73caa714521c6ab
axel22 <aleksandar.prokopec@gmail.com>
2016-07-09 14:39:59 +0200

    Remove quotes.

--- tools/validate-pr.sh
+++ tools/validate-pr.sh
@@ -6,9 +6,9 @@ set -e
 TRAVIS_SCALA_VERSION_ARG=$1
 
 if [ "$TRAVIS_SCALA_VERSION_ARG" == 2.10* ]; then
-  COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG 'reactors210/test'"
+  COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG reactors210/test"
 else
-  COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG 'test'"
+  COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG test"
 fi
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ];
