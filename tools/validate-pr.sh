a9b8f53c44aa4ad6075ba9cb39156e0b5ceab503
axel22 <aleksandar.prokopec@gmail.com>
2016-07-09 15:02:31 +0200

    Fix condition.

--- tools/validate-pr.sh
+++ tools/validate-pr.sh
@@ -5,7 +5,7 @@ set -e
 
 TRAVIS_SCALA_VERSION_ARG=$1
 
-if [ "$TRAVIS_SCALA_VERSION_ARG" == 2.10* ]; then
+if [[ "$TRAVIS_SCALA_VERSION_ARG" == 2.10* ]]; then
   COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG reactors210/test"
 else
   COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG test"
