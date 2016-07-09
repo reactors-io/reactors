443d94748db3e485f7847d1ac7d29c33f6373060
axel22 <aleksandar.prokopec@gmail.com>
2016-07-09 14:32:12 +0200

    Fix.

--- tools/validate-pr.sh
+++ tools/validate-pr.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 set +x
 set -e
 
 TRAVIS_SCALA_VERSION_ARG=$1
 
-if [[ $TRAVIS_SCALA_VERSION_ARG == 2.10* ]]; then
-  COMMAND=sbt ++$TRAVIS_SCALA_VERSION_ARG 'reactors210/test'
+if [ "$TRAVIS_SCALA_VERSION_ARG" == 2.10* ]; then
+  COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG 'reactors210/test'"
 else
-  COMMAND=sbt ++$TRAVIS_SCALA_VERSION_ARG 'test'
+  COMMAND="sbt ++$TRAVIS_SCALA_VERSION_ARG 'test'"
 fi
 
-if [ $TRAVIS_PULL_REQUEST = "false" ];
+if [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
   $COMMAND
 else
