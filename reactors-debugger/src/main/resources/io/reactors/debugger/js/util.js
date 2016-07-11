50c5bd622919d89be2bcd8f3a4b3df71bdce907c
axel22 <aleksandar.prokopec@gmail.com>
2016-07-11 20:58:03 +0200

    Add datatables.

--- reactors-debugger/src/main/resources/io/reactors/debugger/js/util.js
+++ reactors-debugger/src/main/resources/io/reactors/debugger/js/util.js
@@ -7,3 +7,19 @@ class Log {
     throw new Error(err);
   }
 }
+
+
+class UidGenerator {
+  constructor() {
+    this.count = 0;
+  }
+
+  num() {
+    var uid = this.count;
+    this.count += 1;
+    return uid;
+  }
+}
+
+
+var Uid = new UidGenerator();
