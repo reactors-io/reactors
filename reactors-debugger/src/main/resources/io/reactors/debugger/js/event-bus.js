f9975afb6fc1c39c10ae9545893ecc272773b106
axel22 <aleksandar.prokopec@gmail.com>
2016-07-17 02:40:48 +0200

    Add event bus for posting events globally.

---
+++ reactors-debugger/src/main/resources/io/reactors/debugger/js/event-bus.js
@@ -0,0 +1,33 @@
+"use-strict";
+
+
+class EventBus {
+  constructor() {
+    this.uidCount = 0;
+    this.observers = {};
+  }
+
+  observe(key, f) {
+    if (!(key in observers)) {
+      this.observers[key] = {};
+    }
+
+    var obs = this.observers[key];
+    var uid = this.uidCount++;
+    obs[uid] = f;
+
+    return () => {
+      delete obs[uid];
+    }
+  }
+
+  post(key, event) {
+    if (key in this.observers) {
+      var obs = this.observers[key];
+      for (uid in obs) {
+        var f = obs[uid];
+        f(event);
+      }
+    }
+  }
+}
