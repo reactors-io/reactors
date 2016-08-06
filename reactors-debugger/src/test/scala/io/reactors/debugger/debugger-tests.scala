2adf698265b4f8bbbf8712bab66fe456190632b6
Tibor Lotz <tibor.lotz@gmail.com>
2016-08-06 23:54:06 +0200

    Add shell test for starting a reactor.

--- reactors-debugger/src/test/scala/io/reactors/debugger/debugger-tests.scala
+++ reactors-debugger/src/test/scala/io/reactors/debugger/debugger-tests.scala
@@ -5,6 +5,7 @@ package debugger
 
 import org.openqa.selenium._
 import org.openqa.selenium.chrome._
+import org.openqa.selenium.interactions._
 import org.openqa.selenium.support.ui._
 
 
@@ -26,19 +27,57 @@ object DebuggerTest {
     val bundle = new ReactorSystem.Bundle(Scheduler.default, config)
     val system = new ReactorSystem("web-debugger-test-system", bundle)
 
-    // Run tests.
-    runTests(driver, system)
-
-    // Quit.
-    Thread.sleep(2000)
-    driver.quit()
-    system.shutdown()
-    Thread.sleep(1000)
-    System.exit(0)
+    var error: Throwable = null
+    try {
+      // Run tests.
+      runTests(driver, system)
+    } catch {
+      case t: Throwable =>
+        t.printStackTrace()
+        error = t
+    } finally {
+      // Quit.
+      Thread.sleep(2000)
+      driver.quit()
+      system.shutdown()
+      Thread.sleep(1000)
+      if (error == null) System.exit(0)
+      else System.exit(1)
+    }
   }
 
   def runTests(driver: WebDriver, system: ReactorSystem) {
     driver.get("localhost:8888")
-    Thread.sleep(2000)
+
+    // Run shell tests.
+    runShellTests(driver, system)
+  }
+
+  def runShellTests(driver: WebDriver, system: ReactorSystem) {
+    val shellButton = driver.findElement(By.id("x-debugger-button-shell"))
+    shellButton.click()
+
+    // TODO: Replace with waiting for the shell to become ready.
+    Thread.sleep(5000)
+
+    // Create temporary reactor.
+    val shellContainer = driver.findElement(By.className("x-shell-container"))
+    shellContainer.click()
+
+    def shellCommand(text: String) {
+      val cmdline = driver.findElement(By.className("cmd"))
+      val actions = new Actions(driver)
+      actions.moveToElement(cmdline)
+      actions.click()
+      actions.sendKeys(text)
+      actions.sendKeys(Keys.RETURN)
+      actions.build().perform()
+    }
+
+    shellCommand("import scala.concurrent.duration._")
+    shellCommand("val ch = system.spawn(Reactor[String] { self => })")
+
+    // Wait for easier debugging.
+    Thread.sleep(1000)
   }
 }
