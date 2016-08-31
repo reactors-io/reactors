5dea2d420a874acb18749149f7ded709ddc00b64
Cantor Chron <cantor.chron@zoho.com>
2016-09-01 00:04:29 +0200

    WIP on the backpressure protocol.

--- reactors-protocols/src/main/scala/io/reactors/protocol/BackpressureProtocols.scala
+++ reactors-protocols/src/main/scala/io/reactors/protocol/BackpressureProtocols.scala
@@ -12,20 +12,44 @@ trait BackpressureProtocols {
   self: ServerProtocols =>
 
   object Backpressure {
-    type Server[T] = self.Server[Unit, Link[T]]
-    type Req[T] = self.Server.Req[Unit, Link[T]]
+    type Server[T] = self.Server[Channel[Long], Channel[T]]
+    type Req[T] = self.Server.Req[Channel[Long], Channel[T]]
 
-    class Link[T](private val channel: Channel[T]) extends Serializable {
+    class Link[T](
+      private val channel: Channel[T],
+      private val budget: Events[Long]
+    ) extends Serializable {
       def available: Signal[Boolean] = ???
       def trySend(x: T): Boolean = ???
     }
   }
 
   implicit class BackpressureSystemOps(val system: ReactorSystem) {
-    def backpressure[T]: Backpressure.Server[T] = ???
+    def backpressure[T](f: Events[T] => Unit): Backpressure.Server[T] =
+      system.spawn(Reactor[Backpressure.Req[T]] { self =>
+        f(self.main.pressurize().events)
+      })
   }
 
   implicit class BackpressureChannelBuilderOps(val builder: ChannelBuilder) {
-    def backpressure[T]: Connector[Backpressure.Req[T]] = ???
+    def backpressure[T]: Connector[Backpressure.Req[T]] =
+      builder.open[Backpressure.Req[T]]
+  }
+
+  implicit class BackpressureConnectorOps[T](val conn: Connector[Backpressure.Req[T]]) {
+    def pressurize(): Connector[Backpressure.Req[T]] = {
+      ???
+      conn
+    }
+  }
+
+  implicit class BackpressureServerOps[T](val server: Backpressure.Server[T]) {
+    def link: IVar[Backpressure.Link[T]] = {
+      val system = Reactor.self.system
+      val budget = system.channels.daemon.open[Long]
+      (server ? budget.channel).map {
+        ch => new Backpressure.Link(ch, budget.events)
+      }.toIVar
+    }
   }
 }
