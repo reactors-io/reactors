bda17b1c52966cabd059de538f338cc1272f4978
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2016-09-11 05:18:27 +0200

    Add check for the "for-all" backpressure.

--- reactors-protocols/src/test/scala/io/reactors/protocol/backpressure-protocols-tests.scala
+++ reactors-protocols/src/test/scala/io/reactors/protocol/backpressure-protocols-tests.scala
@@ -3,6 +3,10 @@ package protocol
 
 
 
+import io.reactors.test._
+import org.scalacheck._
+import org.scalacheck.Prop.forAllNoShrink
+import org.scalacheck.Gen.choose
 import org.scalatest._
 import scala.collection._
 import scala.concurrent.Await
@@ -70,5 +74,46 @@ class BackpressureProtocolsSpec extends FunSuite {
     }
     val result = Await.result(done.future, 10.seconds).toSet
     assert(result == (0 until 15).map(_.toString).toSet)
+  }
+}
+
+
+class BackpressureProtocolsCheck
+extends Properties("BackpressureProtocolsCheck") with ExtendedProperties {
+  val system = ReactorSystem.default("check-system")
+
+  val sizes = detChoose(0, 50)
+
+  property("backpressure for all") = forAllNoShrink(sizes) {
+    num =>
+    stackTraced {
+      val done = Promise[Seq[String]]()
+      val server = system.backpressureForAll[String](3) { events =>
+        val seen = mutable.Buffer[String]()
+        events onEvent { s =>
+          seen += s
+          if (seen.length == num * 5) {
+            done.success(seen)
+            Reactor.self.main.seal()
+          }
+        }
+      }
+      for (j <- 0 until num) system.spawnLocal[Long] { self =>
+        server.link onEvent { link =>
+          def traverse(i: Int) {
+            if (i == 5) {
+              self.main.seal()
+            } else {
+              val x = j * 5 + i
+              if (link.trySend(x.toString)) traverse(i + 1)
+              else link.available.once.on(traverse(i))
+            }
+          }
+          traverse(0)
+        }
+      }
+      val result = Await.result(done.future, 10.seconds).toSet
+      result == (0 until (num * 5)).map(_.toString).toSet
+    }
   }
 }
