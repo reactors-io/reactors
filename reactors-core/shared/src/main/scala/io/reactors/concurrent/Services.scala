1f3092531513e25060a8337d9f7c39f8c0406d87
Cantor Chron <cantor.chron@zoho.com>
2016-09-30 00:32:58 +0200

    Fix typing.

--- reactors-core/shared/src/main/scala/io/reactors/concurrent/Services.scala
+++ reactors-core/shared/src/main/scala/io/reactors/concurrent/Services.scala
@@ -58,7 +58,8 @@ abstract class Services extends Platform.Reflectable {
   def service[T <: Protocol.Service: ClassTag] = {
     val tag = implicitly[ClassTag[T]]
     if (!services.contains(tag)) {
-      services(tag) = Platform.Reflect.instantiate(tag.runtimeClass, Seq(system))
+      services(tag) =
+        Platform.Reflect.instantiate(tag.runtimeClass, Seq(system)).asInstanceOf[AnyRef]
     }
     services(tag).asInstanceOf[T]
   }
