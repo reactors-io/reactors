bfed840a376451a28e0ace4ad841900e44d206c6
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2016-10-09 00:47:41 +0200

    Clarify tutorial.

--- reactors/jvm/src/test/scala/tutorial/index.scala
+++ reactors/jvm/src/test/scala/tutorial/index.scala
@@ -13,15 +13,30 @@ permalink: /reactors/index.html
 To get started with Reactors.IO, you should grab the latest snapshot version distributed
 on Maven. If you are using SBT, add the following to your project definition:
 
-    resolvers ++= Seq(
-      "Sonatype OSS Snapshots" at
-        "https://oss.sonatype.org/content/repositories/snapshots",
-      "Sonatype OSS Releases" at
-        "https://oss.sonatype.org/content/repositories/releases"
-    )
+```scala
+resolvers ++= Seq(
+  "Sonatype OSS Snapshots" at
+    "https://oss.sonatype.org/content/repositories/snapshots",
+  "Sonatype OSS Releases" at
+    "https://oss.sonatype.org/content/repositories/releases"
+)
 
-    libraryDependencies ++= Seq(
-      "com.storm-enroute" %% "reactors" % "0.7-SNAPSHOT")
+libraryDependencies ++= Seq(
+  "io.reactors" %% "reactors" % "0.8-SNAPSHOT")
+```
+
+If you are using Scala.js, use the following dependency:
+
+```scala
+libraryDependencies ++= Seq(
+  "io.reactors" %%% "reactors" % "0.8-SNAPSHOT")
+```
+
+Examples in this tutorial work with multiple language frontends: Java, Scala and
+Scala.js. By default, code snippets are shown using the Scala syntax,
+which is equivalent to Scala.js (modulo a few platform-specific differences,
+which are noted in respective cases).
+To show the Java version of each snippet, click on the toggle found below the code.
 
 The getting started guide has the following parts:
 
