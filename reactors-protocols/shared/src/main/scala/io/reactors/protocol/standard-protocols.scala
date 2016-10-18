8c3fa932d7587949694f9a5c6cffb11eaeb92cfd
Cantor Chron <cantor.chron@zoho.com>
2016-10-18 23:39:33 +0200

    Add optional `Stamp` type.

---
+++ reactors-protocols/shared/src/main/scala/io/reactors/protocol/standard-protocols.scala
@@ -0,0 +1,28 @@
+package io.reactors
+package protocol
+
+
+
+
+
+
+trait StandardProtocols {
+  sealed trait Stamp[T] {
+    def isEmpty: Boolean
+    def nonEmpty: Boolean
+    def stamp: Long
+  }
+
+  object Stamp {
+    case class None[T]() extends Stamp[T] {
+      def isEmpty = true
+      def nonEmpty = false
+      def stamp = -1
+    }
+
+    case class Some[T](x: T, stamp: Long) extends Stamp[T] {
+      def isEmpty = false
+      def nonEmpty = true
+    }
+  }
+}
