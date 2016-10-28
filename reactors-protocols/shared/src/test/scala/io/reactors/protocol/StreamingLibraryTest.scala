82186b0f0ebed9deae6e9af290c3ad4fbf2fc4d4
Cantor Chron <cantor.chron@zoho.com>
2016-10-28 17:55:50 +0200

    Fix test stub.

--- reactors-protocols/shared/src/test/scala/io/reactors/protocol/StreamingLibraryTest.scala
+++ reactors-protocols/shared/src/test/scala/io/reactors/protocol/StreamingLibraryTest.scala
@@ -6,6 +6,8 @@ package protocol
 import scala.collection._
 import scala.concurrent._
 import scala.concurrent.duration._
+import org.scalatest._
+import org.scalatest.concurrent.AsyncTimeLimitedTests
 
 
 
@@ -18,5 +20,14 @@ class StreamingLibraryTest extends AsyncFunSuite with AsyncTimeLimitedTests {
 
   test("streaming map") {
     // TODO: Implement test.
+    Future(assert(true))
+  }
+}
+
+
+object StreamingLibraryTest {
+  trait Stream[T] {
+    def map[S](f: T => S): Unit
+    def filter(p: T => Boolean): Unit
   }
 }
