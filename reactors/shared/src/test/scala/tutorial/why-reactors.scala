eccd5ca39bbe44ba3ce0723272e9d91706884de4
Cantor Chron <cantor.chron@zoho.com>
2016-11-09 00:37:34 +0100

    Improve intro page.

--- reactors/shared/src/test/scala/tutorial/why-reactors.scala
+++ reactors/shared/src/test/scala/tutorial/why-reactors.scala
@@ -16,9 +16,7 @@ package tutorial
 
 
 
-import io.reactors._
 import org.scalatest._
-import scala.concurrent.Promise
 import scala.concurrent.ExecutionContext
 
 
@@ -27,16 +25,77 @@ import scala.concurrent.ExecutionContext
 ## What are Reactors?
 
 **Reactors** is a foundational framework for concurrent and distributed systems.
-It allows creating concurrent and distributed applications more easily,
+It allows you to create concurrent and distributed applications more easily,
 by providing correct, robust and composable programming abstractions.
-Based on the *reactor model* for distributed programming,
-Reactors provide means to write distributed and concurrent programs
-that can be subdivided into modular components.
-These modular components can be fused together
-in a way that is not possible with standard actors.
+
+Based on the **reactor model** for distributed programming,
+Reactors allow writing location-transparent programs,
+that can be easily subdivided into modular components.
+At the same time, clear separation between units of concurrency called *reactors*
+makes it easier to reason about concurrent programs.
+
+At the core of this reactor programming model,
+the improved composition is the result of
+a careful integration of the traditional actor model
+and functional reactive programming concepts.
+
+
+### Event-driven
+
+Reactors comprise an event-driven programming model.
+Clients can subscribe and react to event streams,
+which asynchronously produce events.
+This makes the reactor model well-suited for interactive applications such as UIs,
+but also ideal for building distributed software,
+which is typically characterized by latency and asynchrony.
+
+
+### Functional
+
+While the concept of event streams and callbacks
+resembles the principles found in traditional actor systems,
+event streams are first-class, functional values.
+They can be declaratively composed and manipulated
+in a similar fashion as Scala collections or Java streams.
+Reactive, incremental data structures can
+also be transformed with high-level functional operators.
+
+Despite the fact that a declarative approach is usually more concise,
+users can gradually transition to a more functional style.
+In fact, you can decide to blend the two styles
+to the degree that you see fit.
+
+
+### Concurrent and distributed
+
+The reactor model organizes computations
+into basic units of concurrency called reactors.
+Inside each reactor, computation is sequential,
+which simplifies program comprehension and accessing reactor state.
+Separate reactors communicate by exchanging events through channels.
+
 At the same time,
-Reactors retains the advantages of the actor model,
-such as location-transparency.
+the reactor model is location-transparent.
+This means that you can develop and test the program on a single machine,
+and then seamlessly deploy it on multiple machine that are connected
+with a computer network.
+
+
+### Modular
+
+The subtle interplay between channels and event streams
+allows composing powerful message exchange protocols in a modular fashion.
+In addition to a rich library of composable message protocols,
+clients can implement and modularize their own message exchange patterns,
+and later use them in concrete reactors.
+
+
+### Multiple frontends
+
+At the time of writing this document,
+the Reactors framework has bindings for both Scala and Scala.JS,
+as well as Java.
+Additional frontends are planned in the future.
 
 You can download Reactors from
 [http://reactors.io/download/](http://reactors.io/download/).
@@ -74,12 +133,12 @@ While in some cases these issues are partially addressed by some existing framew
 Reactors go a step further.
 In particular, the Reactors framework is based on the following:
 
-- Location-transparent reactors, lightweight entities that execute concurrently with
+- Location-transparent **reactors**, lightweight entities that execute concurrently with
   each other, but are internally always single-threaded,
   and can be ported from a single machine to a distributed setting.
-- Asynchronous first-class event streams that can be reasoned about
+- Asynchronous first-class **event streams** that can be reasoned about
   in a declarative, functional manner, and are the basis for composing components.
-- Channels that can be shared between reactors, and are used to asynchronously
+- **Channels** that can be shared between reactors, and are used to asynchronously
   send events.
 
 These three unique abstractions are the core prerequisite
