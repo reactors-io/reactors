9dc9b6e91e1e971c9c2b7bbd0644e971a049056f
Cantor Chron <cantor.chron@zoho.com>
2017-01-04 19:20:37 +0100

    Add benchmarks for various link protocols.

--- reactors-protocol/jvm/src/bench/scala/io/reactors/protocol/link-protocols-bench.scala
+++ reactors-protocol/jvm/src/bench/scala/io/reactors/protocol/link-protocols-bench.scala
@@ -15,7 +15,7 @@ class LinkProtocolsBench extends JBench.OfflineReport {
   override def defaultConfig = Context(
     exec.minWarmupRuns -> 80,
     exec.maxWarmupRuns -> 160,
-    exec.benchRuns -> 5000,
+    exec.benchRuns -> 50,
     exec.independentSamples -> 1,
     verbose -> true
   )
@@ -28,112 +28,152 @@ class LinkProtocolsBench extends JBench.OfflineReport {
   val maxSize = 200000
   val sizes = Gen.range("size")(maxSize, maxSize, 2000)
 
-  @transient lazy val system = new ReactorSystem("reactor-bench")
-
-  // @gen("sizes")
-  // @benchmark("io.reactors.protocol.link")
-  // @curve("send")
-  // def send(sz: Int): Unit = {
-  //   val done = Promise[Boolean]()
-  //   val ch = system.spawnLocal[Int] { self =>
-  //     var count = 0
-  //     self.main.events onEvent { x =>
-  //       count += 1
-  //       if (count == sz) done.success(true)
-  //     }
-  //   }
-  //   system.spawnLocal[Unit] { self =>
-  //     var i = 0
-  //     while (i < sz) {
-  //       ch ! i
-  //       i += 1
-  //     }
-  //   }
-  //   assert(Await.result(done.future, 10.seconds))
-  // }
-
-  // @gen("sizes")
-  // @benchmark("io.reactors.protocol.link")
-  // @curve("two-way-link")
-  // def twoWaySend(sz: Int): Unit = {
-  //   val done = Promise[Boolean]()
-  //   val server = system.twoWayServer[Int, Int] { (server, link) =>
-  //     var count = 0
-  //     link.input onEvent { x =>
-  //       count += 1
-  //       if (count == sz) done.success(true)
-  //     }
-  //   }
-  //   system.spawnLocal[Unit] { self =>
-  //     server.connect() onEvent { link =>
-  //       var i = 0
-  //       while (i < sz) {
-  //         link.output ! i
-  //         i += 1
-  //       }
-  //     }
-  //   }
-  //   assert(Await.result(done.future, 10.seconds))
-  // }
-
-  // @gen("sizes")
-  // @benchmark("io.reactors.protocol.link")
-  // @curve("reliable-link")
-  // def reliableSend(sz: Int): Unit = {
-  //   val done = Promise[Boolean]()
-  //   val policy = Reliable.Policy.reorder(8192)
-  //   val server = system.reliableServer[String](policy) {
-  //     (server, link) =>
-  //     var count = 0
-  //     link.events onEvent { x =>
-  //       count += 1
-  //       if (count == sz) done.success(true)
-  //     }
-  //   }
-  //   system.spawnLocal[Unit] { self =>
-  //     server.openReliable(policy) onEvent { r =>
-  //       var i = 0
-  //       while (i < sz) {
-  //         r.channel ! "data"
-  //         i += 1
-  //       }
-  //     }
-  //   }
-  //   assert(Await.result(done.future, 10.seconds))
-  // }
-
-  // @gen("sizes")
-  // @benchmark("io.reactors.protocol.link")
-  // @curve("reliable-optimized-link")
-  // def reliableOptimizedSend(sz: Int): Unit = {
-  //   val done = Promise[Boolean]()
-  //   val policy = Reliable.Policy.fastReorder(8192)
-  //   val server = system.reliableServer[String](policy) {
-  //     (server, link) =>
-  //     var count = 0
-  //     link.events onEvent { x =>
-  //       count += 1
-  //       if (count == sz) done.success(true)
-  //     }
-  //   }
-  //   system.spawnLocal[Unit] { self =>
-  //     server.openReliable(policy) onEvent { r =>
-  //       var i = 0
-  //       while (i < sz) {
-  //         r.channel ! "data"
-  //         i += 1
-  //       }
-  //     }
-  //   }
-  //   assert(Await.result(done.future, 10.seconds))
-  // }
+  @transient lazy val system = ReactorSystem.default("reactor-bench", """
+    scheduler = {
+      default = {
+        budget = 8192
+      }
+    }
+  """)
 
   @gen("sizes")
   @benchmark("io.reactors.protocol.link")
-  @curve("backpressure-simple-link")
+  @curve("fire-and-forget")
+  def send(sz: Int): Unit = {
+    val done = Promise[Boolean]()
+    val ch = system.spawnLocal[Int] { self =>
+      var count = 0
+      self.main.events onEvent { x =>
+        count += 1
+        if (count == sz) done.success(true)
+      }
+    }
+    system.spawnLocal[Unit] { self =>
+      var i = 0
+      while (i < sz) {
+        ch ! i
+        i += 1
+      }
+    }
+    assert(Await.result(done.future, 10.seconds))
+  }
+
+  @gen("sizes")
+  @benchmark("io.reactors.protocol.link")
+  @curve("two-way-link")
+  def twoWaySend(sz: Int): Unit = {
+    val done = Promise[Boolean]()
+    val server = system.twoWayServer[Int, Int] { (server, link) =>
+      var count = 0
+      link.input onEvent { x =>
+        count += 1
+        if (count == sz) done.success(true)
+      }
+    }
+    system.spawnLocal[Unit] { self =>
+      server.connect() onEvent { link =>
+        var i = 0
+        while (i < sz) {
+          link.output ! i
+          i += 1
+        }
+      }
+    }
+    assert(Await.result(done.future, 10.seconds))
+  }
+
+  @gen("sizes")
+  @benchmark("io.reactors.protocol.link")
+  @curve("reliable-link")
+  def reliableSend(sz: Int): Unit = {
+    val done = Promise[Boolean]()
+    val policy = Reliable.Policy.reorder(8192)
+    val server = system.reliableServer[String](policy) {
+      (server, link) =>
+      var count = 0
+      link.events onEvent { x =>
+        count += 1
+        if (count == sz) done.success(true)
+      }
+    }
+    system.spawnLocal[Unit] { self =>
+      server.openReliable(policy) onEvent { r =>
+        var i = 0
+        while (i < sz) {
+          r.channel ! "data"
+          i += 1
+        }
+      }
+    }
+    assert(Await.result(done.future, 10.seconds))
+  }
+
+  @gen("sizes")
+  @benchmark("io.reactors.protocol.link")
+  @curve("optimized-reliable-link")
+  def reliableOptimizedSend(sz: Int): Unit = {
+    val done = Promise[Boolean]()
+    val policy = Reliable.Policy.fastReorder
+    val server = system.reliableServer[String](policy) {
+      (server, link) =>
+      var count = 0
+      link.events onEvent { x =>
+        count += 1
+        if (count == sz) done.success(true)
+      }
+    }
+    system.spawnLocal[Unit] { self =>
+      server.openReliable(policy) onEvent { r =>
+        var i = 0
+        while (i < sz) {
+          r.channel ! "data"
+          i += 1
+        }
+      }
+    }
+    assert(Await.result(done.future, 10.seconds))
+  }
+
+  @gen("sizes")
+  @benchmark("io.reactors.protocol.link")
+  @curve("simple-backpressure-link")
   def backpressureSimpleSend(sz: Int): Unit = {
     val done = Promise[Boolean]()
     val medium = Backpressure.Medium.default[Int]
+    val policy = Backpressure.Policy.batching(8192)
+    val server = system.backpressureServer(medium, policy) {
+      case Backpressure.PumpServer(ch, links, sub) =>
+        links onEvent { pump =>
+          var count = 0
+          pump.available.is(true) on {
+            while (pump.available()) {
+              pump.dequeue()
+              count += 1
+              if (count == sz) done.success(true)
+            }
+          }
+        }
+    }
+    system.spawnLocal[Unit] { self =>
+      server.openBackpressure(medium, policy) onEvent { valve =>
+        var i = 0
+        valve.available.is(true) on {
+          while (valve.available() && i < sz) {
+            valve.channel ! 0
+            i += 1
+          }
+        }
+      }
+    }
+    assert(Await.result(done.future, 10.seconds))
+  }
+
+  @gen("sizes")
+  @benchmark("io.reactors.protocol.link")
+  @curve("reliable-backpressure-link")
+  def backpressureReliableSend(sz: Int): Unit = {
+    val done = Promise[Boolean]()
+    val medium = Backpressure.Medium.reliable[Int](Reliable.TwoWay.Policy.fastReorder)
     val policy = Backpressure.Policy.batching(8192)
     val server = system.backpressureServer(medium, policy) {
       case Backpressure.PumpServer(ch, links, sub) =>
