2d31b8545f46c8213ffd8adf858520ed6e3581f4
Cantor Chron <cantor.chron@zoho.com>
2017-01-17 16:19:32 +0100

    Add `rendezvous` protocol.

--- reactors-protocol/shared/src/main/scala/io/reactors/protocol/rendezvous-protocols.scala
+++ reactors-protocol/shared/src/main/scala/io/reactors/protocol/rendezvous-protocols.scala
@@ -10,7 +10,7 @@ import io.reactors.common.UnrolledRing
 /** Scatter-gather communication patterns.
  */
 trait RendezvousProtocols {
-  def rendezvous[T: Arrayable, S: Arrayable]: (Server[T, S], Server[S, T]) = {
+  def rendezvous[T: Arrayable, S: Arrayable]: Rendezvous[T, S] = {
     val builder = Reactor.self.system.channels.daemon
     val (ct, cs) = (builder.open[T], builder.open[S])
     val (qt, qs) = (new UnrolledRing[T], new UnrolledRing[S])
@@ -18,12 +18,20 @@ trait RendezvousProtocols {
       ct.channel ! qt.dequeue()
       cs.channel ! qs.dequeue()
     }
-    def meet[X, Y](x: X, qx: Queue[X], ey: Events[Y]): Events[Y] = {
+    def meet[X, Y](x: X, qx: UnrolledRing[X], ey: Events[Y]): Events[Y] = {
       qx.enqueue(x)
       flush()
       ey
     }
-    val ts = server[T, S].asyncServe(t => meet(t, qt, cs.events))
-    val st = server[S, T].asyncServe(s => meet(s, qs, qt.events))
+    val ts = builder.server[T, S].asyncServe(t => meet(t, qt, cs.events))
+    val st = builder.server[S, T].asyncServe(s => meet(s, qs, ct.events))
+    Rendezvous(ts.channel, st.channel, ts.subscription.chain(st.subscription))
   }
 }
+
+
+case class Rendezvous[T, S](
+  left: Server[T, S],
+  right: Server[S, T],
+  subscription: Subscription
+)
