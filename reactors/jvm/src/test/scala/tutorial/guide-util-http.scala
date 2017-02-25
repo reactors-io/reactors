eaa8e1d33bb225457c04a177a44f09fe9b87d968
Bertrand Kang <bertrand.kang@gmail.com>
2017-02-25 02:51:09 +0100

    Add documentation for parallel servers.

--- reactors/jvm/src/test/scala/tutorial/guide-util-http.scala
+++ reactors/jvm/src/test/scala/tutorial/guide-util-http.scala
@@ -45,7 +45,7 @@ class GuideHttpService extends AsyncFunSuite {
     that requests a specific port. Until that reactor terminates,
     other reactors cannot use that port (but they are allowed to request other ports).
     After a reactor acquires a specific port, it effectively becomes the server
-    at that port. All incoming requests for that port are handled on this reactor.
+    at that port. Inbound requests for that port are by default handled on this reactor.
 
     To reply to user requests, the reactor then needs to define a set of routes.
     Each route specifies how to respond to user requests coming on a different
@@ -82,7 +82,7 @@ class GuideHttpService extends AsyncFunSuite {
     val server = Reactor[Unit] { self =>
       val http = self.system.service[Http]
       http.at(9500).text("/hello") { req =>
-        val name = req.parameters.getOrElse("name", "World").head
+        val name = req.parameters.getOrElse("name", Seq("World")).head
         s"Hello, $name."
       }
       http.at(9500).html("/about") { req =>
@@ -124,11 +124,56 @@ class GuideHttpService extends AsyncFunSuite {
     /*!end-code!*/
 
     /*!md
+    If necessary, you can assign a custom scheduler to your server reactor.
+    For example, if you want to ensure that your server gets a high priority,
+    you can use the custom thread scheduler. See the Schedulers section
+    for more details.
+
     You can now point your browser to `http://localhost:9500/hello`,
     and see your web page in action.
+
+
+    ## Parallel Servers
+
+    It is also possible to create a server that forwards incoming messages
+    to other workers. In this case, the parallelism of the server is improved,
+    but the workers are nort allowed to access the local state of the reactor.
+    To create such a server, we need to pass the worker channel to the `at` method
+    the first time it gets called.
+
+    We start by creating a batch of workers -- we will import the `io.reactors.protocol`
+    package to more easily create the workers and combine their input channels
+    (see the Protocols section for more details):
     !*/
 
-    Thread.sleep(2500)
+    /*!begin-code!*/
+    val parallelServer = Reactor[Unit] { self =>
+      import io.reactors.protocol._
+      val workers = for (i <- 0 until 8) yield
+        system.spawnLocal[Http.Connection] {
+          self => self.main.events.onEvent(_.accept())
+        }
+      val workerChannel = self.system.channels.router[Http.Connection]
+        .route(Router.roundRobin(workers)).channel
+    /*!end-code!*/
+
+    /*!md
+    Now that we have our worker list, we can create the parallel server instance:
+    !*/
+
+    /*!begin-code!*/
+      self.system.service[Http].at(9502)
+        .text("/round") { req => "Round and round it goes!" }
+    }
+    /*!end-code!*/
+
+    /*!md
+    Again, note that the handler for `/round` is not allowed to access any local
+    reactor state in this example, since the handler will be invoked in parallel
+    by different workers.
+    !*/
+
+    Thread.sleep(25000)
     system.shutdown()
 
     Promise.successful(assert(true)).future
