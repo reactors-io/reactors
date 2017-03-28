a8e88f25dab5f17ae045b7cc2ac760b8a980e8b6
Bertrand Kang <bertrand.kang@gmail.com>
2017-03-28 22:11:23 +0200

    Add double array marshaling support.

--- reactors-remote/jvm/src/test/scala/io/reactors/remote/runtime-marshaler-tests.scala
+++ reactors-remote/jvm/src/test/scala/io/reactors/remote/runtime-marshaler-tests.scala
@@ -323,6 +323,18 @@ class RuntimeMarshalerTest extends FunSuite {
     assert(obj.array.length == 256)
     for (i <- 0 until 256) assert(obj.array(i) == i + 3)
   }
+
+  test("marshal an object with a double array") {
+    val data = new Data.Linked(128, 128)
+    val cell = new Cell[Data](data)
+    val input = new DoubleArrayObject(256)
+    for (i <- 0 until 256) input.array(i) = i + 3
+    RuntimeMarshaler.marshal(input, data)
+    println(data.byteString)
+    val obj = RuntimeMarshaler.unmarshal[DoubleArrayObject](cell)
+    assert(obj.array.length == 256)
+    for (i <- 0 until 256) assert(obj.array(i) == i + 3)
+  }
 }
 
 
@@ -387,4 +399,8 @@ class VarArrayObject(var array: Array[Int])
 
 class LongArrayObject(length: Int) {
   val array = new Array[Long](length)
+}
+
+class DoubleArrayObject(length: Int) {
+  val array = new Array[Double](length)
 }
