d8a6d0997d302d85865cd8a0000945c61e75cd19
Bertrand Kang <bertrand.kang@gmail.com>
2017-05-08 22:25:55 +0200

    Uncomment benchmarks.

--- reactors-remote/jvm/src/bench/scala/io/reactors/remote/runtime-marshaler-bench.scala
+++ reactors-remote/jvm/src/bench/scala/io/reactors/remote/runtime-marshaler-bench.scala
@@ -51,64 +51,64 @@ class RuntimeMarshalerBench extends JBench.OfflineReport {
 
   @transient lazy val system = new ReactorSystem("reactor-bench")
 
-//  @gen("noSizes")
-//  @benchmark("io.reactors.remote.runtime-marshaler")
-//  @curve("serialize-final-single-field-class")
-//  def serializeFinalSingleFieldClass(bufferSize: Int) = {
-//    var i = 0
-//    val baos = new ByteArrayOutputStream()
-//    val oos = new ObjectOutputStream(baos)
-//    while (i < repetitions) {
-//      val obj = new SingleField(i)
-//      oos.writeObject(obj)
-//      i += 1
-//    }
-//  }
-//
-//  @gen("bufferSizes")
-//  @benchmark("io.reactors.remote.runtime-marshaler")
-//  @curve("write-final-single-field-class")
-//  def writeFinalSingleFieldClass(bufferSize: Int) = {
-//    var i = 0
-//    var data: Data = new Data.Linked(bufferSize, bufferSize)
-//    val cell = new Cell[Data](data)
-//    while (i < repetitions) {
-//      val obj = new SingleField(i)
-//      val v = obj.x
-//      if (data.remainingWriteSize < 4) data = data.flush(4)
-//      val pos = data.endPos
-//      data(pos + 0) = ((v & 0x000000ff) >>> 0).toByte
-//      data(pos + 1) = ((v & 0x0000ff00) >>> 8).toByte
-//      data(pos + 2) = ((v & 0x00ff0000) >>> 16).toByte
-//      data(pos + 3) = ((v & 0xff000000) >>> 24).toByte
-//      data.endPos += 4
-//      if (data.remainingWriteSize < 4) data = data.flush(4)
-//      val v1 = 1
-//      val pos1 = data.endPos
-//      data(pos1 + 0) = ((v1 & 0x000000ff) >>> 0).toByte
-//      data(pos1 + 1) = ((v1 & 0x0000ff00) >>> 8).toByte
-//      data(pos1 + 2) = ((v1 & 0x00ff0000) >>> 16).toByte
-//      data(pos1 + 3) = ((v1 & 0xff000000) >>> 24).toByte
-//      data.endPos += 4
-//      i += 1
-//    }
-//    data
-//  }
+  @gen("noSizes")
+  @benchmark("io.reactors.remote.runtime-marshaler")
+  @curve("serialize-final-single-field-class")
+  def serializeFinalSingleFieldClass(bufferSize: Int) = {
+    var i = 0
+    val baos = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(baos)
+    while (i < repetitions) {
+      val obj = new SingleField(i)
+      oos.writeObject(obj)
+      i += 1
+    }
+  }
 
-//  @gen("bufferSizes")
-//  @benchmark("io.reactors.remote.runtime-marshaler")
-//  @curve("marshal-final-single-field-class")
-//  def marshalFinalSingleFieldClass(bufferSize: Int) = {
-//    var i = 0
-//    var data: Data = new Data.Linked(bufferSize, bufferSize)
-//    val desc = Platform.Reflect.descriptorOf(classOf[SingleField])
-//    while (i < repetitions) {
-//      val obj = new SingleField(i)
-//      data = RuntimeMarshaler.marshalAs(desc, obj, data, false)
-//      i += 1
-//    }
-//    data
-//  }
+  @gen("bufferSizes")
+  @benchmark("io.reactors.remote.runtime-marshaler")
+  @curve("write-final-single-field-class")
+  def writeFinalSingleFieldClass(bufferSize: Int) = {
+    var i = 0
+    var data: Data = new Data.Linked(bufferSize, bufferSize)
+    val cell = new Cell[Data](data)
+    while (i < repetitions) {
+      val obj = new SingleField(i)
+      val v = obj.x
+      if (data.remainingWriteSize < 4) data = data.flush(4)
+      val pos = data.endPos
+      data(pos + 0) = ((v & 0x000000ff) >>> 0).toByte
+      data(pos + 1) = ((v & 0x0000ff00) >>> 8).toByte
+      data(pos + 2) = ((v & 0x00ff0000) >>> 16).toByte
+      data(pos + 3) = ((v & 0xff000000) >>> 24).toByte
+      data.endPos += 4
+      if (data.remainingWriteSize < 4) data = data.flush(4)
+      val v1 = 1
+      val pos1 = data.endPos
+      data(pos1 + 0) = ((v1 & 0x000000ff) >>> 0).toByte
+      data(pos1 + 1) = ((v1 & 0x0000ff00) >>> 8).toByte
+      data(pos1 + 2) = ((v1 & 0x00ff0000) >>> 16).toByte
+      data(pos1 + 3) = ((v1 & 0xff000000) >>> 24).toByte
+      data.endPos += 4
+      i += 1
+    }
+    data
+  }
+
+  @gen("bufferSizes")
+  @benchmark("io.reactors.remote.runtime-marshaler")
+  @curve("marshal-final-single-field-class")
+  def marshalFinalSingleFieldClass(bufferSize: Int) = {
+    var i = 0
+    var data: Data = new Data.Linked(bufferSize, bufferSize)
+    val desc = Platform.Reflect.descriptorOf(classOf[SingleField])
+    while (i < repetitions) {
+      val obj = new SingleField(i)
+      data = RuntimeMarshaler.marshalAs(desc, obj, data, false)
+      i += 1
+    }
+    data
+  }
 
   def setupDeserializeFinalSingleFieldClass(cell: Cell[ObjectInputStream]) = {
     var i = 0
@@ -124,21 +124,21 @@ class RuntimeMarshalerBench extends JBench.OfflineReport {
     cell := ois
   }
 
-//  @gen("inputStreams")
-//  @benchmark("io.reactors.remote.runtime-unmarshaler")
-//  @curve("deserialize-final-single-field-class")
-//  @setup("setupDeserializeFinalSingleFieldClass")
-//  def deserializeFinalSingleFieldClass(cell: Cell[ObjectInputStream]) = {
-//    val ois = cell()
-//    var i = 0
-//    var sum = 0L
-//    while (i < repetitions) {
-//      val obj = ois.readObject().asInstanceOf[SingleField]
-//      sum += obj.x
-//      i += 1
-//    }
-//    sum
-//  }
+  @gen("inputStreams")
+  @benchmark("io.reactors.remote.runtime-unmarshaler")
+  @curve("deserialize-final-single-field-class")
+  @setup("setupDeserializeFinalSingleFieldClass")
+  def deserializeFinalSingleFieldClass(cell: Cell[ObjectInputStream]) = {
+    val ois = cell()
+    var i = 0
+    var sum = 0L
+    while (i < repetitions) {
+      val obj = ois.readObject().asInstanceOf[SingleField]
+      sum += obj.x
+      i += 1
+    }
+    sum
+  }
 
   def setupUnmarshalFinalSingleFieldClass(data: Data): Unit = {
     var curr = data.asInstanceOf[Data.Linked]
