3d48b2d87d72646e726fddf1448267ecd6f2b8cc
Bertrand Kang <bertrand.kang@gmail.com>
2017-06-18 01:43:05 +0200

    Add alternative ctor for ValArray.

--- reactors-common/shared/src/main/scala/io/reactors/common/ValArray.scala
+++ reactors-common/shared/src/main/scala/io/reactors/common/ValArray.scala
@@ -2,13 +2,20 @@ package io.reactors.common
 
 
 
+import scala.reflect.ClassTag
 
 
 
-class ValArray[@specialized(Byte, Char, Int, Long, Float, Double) T](
+class ValArray[@specialized(Byte, Char, Int, Long, Float, Double) T: ClassTag](
   private var raw: Array[T]
 ) {
+  def this(len: Int) = this(new Array[T](len))
+
   def apply(i: Int): T = raw(i)
 
   def length: Int = raw.length
+}
+
+
+object ValArray {
 }
