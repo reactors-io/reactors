19646a315c4352eea5e43caacbe9360ab51d343e
Fyodor Fedorowicz <fyodor.fedorowicz@gmail.com>
2018-01-01 02:35:30 +0100

    Add permute tree.

---
+++ reactors-common/jvm/src/main/scala/io/reactors/common/concurrent/PermuteTree.scala
@@ -0,0 +1,17 @@
+package io.reactors.common.concurrent
+
+
+
+import sun.misc.Unsafe
+import scala.annotation.switch
+import scala.annotation.tailrec
+
+
+
+class PermuteTree[K <: AnyRef, V <: AnyRef] {
+  private def unsafe: Unsafe = Platform.unsafe
+}
+
+
+object PermuteTree {
+}
