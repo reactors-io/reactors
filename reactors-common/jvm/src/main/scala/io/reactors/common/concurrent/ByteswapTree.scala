3f89d87525ed167077359c1d856c1a28af43e236
Fyodor Fedorowicz <fyodor.fedorowicz@gmail.com>
2018-01-14 23:05:13 +0100

    WIP on the leaf insert operation.

--- reactors-common/jvm/src/main/scala/io/reactors/common/concurrent/ByteswapTree.scala
+++ reactors-common/jvm/src/main/scala/io/reactors/common/concurrent/ByteswapTree.scala
@@ -15,8 +15,27 @@ class ByteswapTree[K <: AnyRef, V <: AnyRef] {
 
   private def unsafe: Unsafe = Platform.unsafe
 
-  private def READ_PERMUTATION(leaf: Leaf): Long = {
-    unsafe.getLongVolatile(leaf, ???)
+  private def COUNT_MASK: Long = 0xf000000000000000L
+
+  private def COUNT_SHIFT: Int = 60
+
+  private def READ_MASK(leaf: Leaf): Long = {
+    unsafe.getLongVolatile(leaf, LeafMaskOffset)
+  }
+
+  private def CAS_PERMUTATION(leaf: Leaf, ov: Long, nv: Long): Boolean = {
+    unsafe.compareAndSwapLong(leaf, LeafMaskOffset, ov, nv)
+  }
+
+  private def READ_ENTRY(leaf: Leaf, idx: Int): Entry[K, V] = {
+    unsafe.getObjectVolatile(leaf, LeafEntryOffset + idx * LeafEntryScaling)
+      .asInstanceOf[Entry[K, V]]
+  }
+
+  private def CAS_ENTRY(
+    leaf: Leaf, idx: Int, ov: Entry[K, V], nv: Entry[K, V]
+  ): Boolean = {
+    unsafe.compareAndSwapObject(leaf, LeafEntryOffset + idx * LeafEntryScaling, ov, nv)
   }
 
   private def insert(k: K, v: V): Unit = {
@@ -24,13 +43,46 @@ class ByteswapTree[K <: AnyRef, V <: AnyRef] {
   }
 
   private def insert(leaf: Leaf, k: K, v: V): Unit = {
-    ???
+    // Determine node state.
+    val mask = READ_MASK(leaf)
+    val count = ((mask >>> COUNT_SHIFT) & COUNT_MASK).toInt
+
+    // Determine the new mask: position for the key, and whether to replace a key.
+    val newMask: Int = ???
+
+    // Attempt to propose the next key.
+    val entry = new Entry(k, v)
+    if (CAS_ENTRY(leaf, count, null, entry)) {
+      ???
+    } else {
+      // Help complete an already proposed key.
+      ???
+
+      // Retry.
+    }
   }
 }
 
 
 object ByteswapTree {
-  private def layoutCheck(cls: Class[_], rootName: String): FieldOffsets = {
+  private def offset(cls: Class[_], fieldName: String): Long = {
+    val field = cls.getDeclaredField(fieldName)
+    Platform.unsafe.objectFieldOffset(field)
+  }
+
+  private[concurrent] val LeafMaskOffset = offset(classOf[Leaf], "mask")
+  private[concurrent] val LeafEntryOffset = offset(classOf[Leaf], "entry0")
+  private[concurrent] val LeafEntryScaling =
+    offset(classOf[Leaf], "entry1") - offset(classOf[Leaf], "entry0")
+  private[concurrent] val InnerMaskOffset = offset(classOf[Inner], "mask")
+  private[concurrent] val InnerEntryOffset = offset(classOf[Inner], "entry0")
+  private[concurrent] val InnerEntryScaling =
+    offset(classOf[Inner], "entry1") - offset(classOf[Inner], "entry0")
+  private[concurrent] val InnerKeyOffset = offset(classOf[Inner], "key0")
+  private[concurrent] val InnerKeyScaling =
+    offset(classOf[Inner], "key1") - offset(classOf[Inner], "key0")
+
+  private def layoutCheck(cls: Class[_], rootName: String): Unit = {
     def offset(fieldName: String): Long = {
       val field = cls.getDeclaredField(fieldName)
       Platform.unsafe.objectFieldOffset(field)
@@ -52,20 +104,16 @@ object ByteswapTree {
     for (i <- 1 until 15) {
       compareOffsets(i, rootName + i)
     }
-
-    FieldOffsets(firstOffset, scalingFactor)
   }
 
-  case class FieldOffsets(start: Long, scalingFactor: Long)
-
-  val leafEntryFields = layoutCheck(classOf[Leaf], "entry")
-  val innerEntryFields = layoutCheck(classOf[Inner], "entry")
-  val innerKeyFields = layoutCheck(classOf[Inner], "key")
+  layoutCheck(classOf[Leaf], "entry")
+  layoutCheck(classOf[Inner], "entry")
+  layoutCheck(classOf[Inner], "key")
 
   abstract class Node
 
   class Leaf extends Node {
-    @volatile var permutation: Long = 0
+    @volatile var mask: Long = 0
     @volatile var unused0: AnyRef = null
     @volatile var entry0: AnyRef = null
     @volatile var entry1: AnyRef = null
@@ -85,7 +133,7 @@ object ByteswapTree {
   }
 
   class Inner extends Node {
-    @volatile var permutation: Long = 0
+    @volatile var mask: Long = 0
     @volatile var unused0: AnyRef = null
     @volatile var entry0: AnyRef = null
     @volatile var entry1: AnyRef = null
@@ -118,4 +166,6 @@ object ByteswapTree {
     @volatile var key13: AnyRef = null
     @volatile var key14: AnyRef = null
   }
+
+  class Entry[K <: AnyRef, V <: AnyRef](val key: K, val value: V)
 }
